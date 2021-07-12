cd29806a7d35c9ab9d17c422a48f21352040fe4f
Cipriano99 <samuelcipriano99@gmail.com>
2021-07-12 02:55:09 -0300

    Sequelize and Postgres configured

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const db = require('./models');
 
 const authController = require('./controllers/auth');
 
@@ -11,4 +12,6 @@ app.get('/', (req, res) => {
   return res.json('Api runing');
 });
 
-app.listen(3001, () => console.log(`Listening on port 3001 🔥`));
+db.sequelize.sync().then(() => {
+  app.listen(3001, () => console.log(`Listening on port 3001 🔥`));
+});

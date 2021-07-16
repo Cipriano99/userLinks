a8ee91bf8eaa61534318dedced8f3fc307d54f66
Cipriano99 <samuelcipriano99@gmail.com>
2021-07-15 22:09:43 -0300

    Verify JWT

--- src/controllers/link.js
+++ src/controllers/link.js
@@ -5,14 +5,14 @@ const { Link } = require('../models');
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-  const accountId = 10; // req.id
+  const { accountId } = req;
   const links = await Link.findAll({ where: { accountId } });
 
   return res.jsonOK(links);
 });
 
 router.get('/:id', async (req, res) => {
-  const accountId = 10; // req.id
+  const { accountId } = req;
   const { id } = req.params;
 
   const link = await Link.findOne({ where: { id, accountId } });
@@ -22,8 +22,8 @@ router.get('/:id', async (req, res) => {
 });
 
 router.post('/', async (req, res) => {
-  const accountId = 8; // req.id
-  const { label, url, isSocial } = req.body;
+  const { accountId, body } = req;
+  const { label, url, isSocial } = body;
 
   const image = 'linkImage';
 
@@ -33,9 +33,8 @@ router.post('/', async (req, res) => {
 });
 
 router.put('/:id', async (req, res) => {
-  const accountId = 8; // req.id
+  const { accountId, body } = req;
   const { id } = req.params;
-  const { body } = req;
 
   const fields = ['label', 'url', 'isSocial'];
 
@@ -54,7 +53,7 @@ router.put('/:id', async (req, res) => {
 });
 
 router.delete('/:id', async (req, res) => {
-  const accountId = 8; // req.id
+  const { accountId } = req;
   const { id } = req.params;
 
   const link = await Link.findOne({ where: { id, accountId } });

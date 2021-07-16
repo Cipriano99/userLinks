24242aa49687f11593dedcb1ea622eaf00a8ed91
Cipriano99 <samuelcipriano99@gmail.com>
2021-07-15 22:19:52 -0300

    Excluded auth paths from JWT

--- src/middlewares/jwt.js
+++ src/middlewares/jwt.js
@@ -1,6 +1,14 @@
 const { verifyJwt } = require('../helpers/jsonwebtoken');
 
 const checkJwt = (req, res, next) => {
+  const { url: path } = req;
+
+  const excludedPaths = ['/auth/signin', '/auth/signup'].find((p) =>
+    p.startsWith(path)
+  );
+  const isExcluded = !!excludedPaths.find((p) => p.startsWith(path));
+  if (isExcluded) return next();
+
   let token = req.headers['authorization'];
   token = token ? token.slice(7, token.length) : null;
   if (!token) return res.jsonUnauthorized(null, 'Invalid token');
